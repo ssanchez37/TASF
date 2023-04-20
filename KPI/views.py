b0355447237df5628bc72993fc672c08e95e2c36
Sergio Daniel Sanchez <64474204+ssanchez37@users.noreply.github.com>
2023-04-19 20:13:31 -0600

    Compatibilidad con Planes de Web (#1)

--- KPI/views.py
+++ KPI/views.py
@@ -13,6 +13,7 @@ def simple_upload(request):
         myfile = request.FILES['wrike_file']
         budget = request.POST['budget']
         mandays = request.POST['mandays']
+        area = request.POST['area-tasf']
         fs = FileSystemStorage()
         filename = fs.save(myfile.name, myfile)
         uploaded_file_url = fs.url(filename)
@@ -22,7 +23,8 @@ def simple_upload(request):
                 output, l1, inc_tot, inc_time_tot, t1, inc_time_ind, inc_cost_tot, t2, inc_cost_ind, t3, cc_time_tot,\
                     t4, cc_time_ind, cc_cost_tot, t5, cc_cost_ind, t6, bcws_costo, ev_costo, acwp_costo, pr_costo, \
                     pf_costo, cv_costo, t7, bcws_time, ev_time, acwp_time, pr_time, pf_time, sv_time, mandays_tiempo, \
-                    ev_mandays_time, pr_mandays_tiempo, pf_mandays_time, sv_mandays_time = kpis(myfile, budget, mandays)
+                    ev_mandays_time, pr_mandays_tiempo, pf_mandays_time, sv_mandays_time = kpis(myfile, budget, mandays,
+                                                                                                area)
                 os.remove((os.path.join(MEDIA_ROOT + '/' + filename)))
             except sp.CalledProcessError as e:
                 print(e)
